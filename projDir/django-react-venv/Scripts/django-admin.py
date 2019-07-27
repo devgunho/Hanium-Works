c3e83f2312355e1ef4aa23d48ec54dcc5c6557eb
gunho <parkgh3737@naver.com>
2019-07-27 16:06:45 +0900

    django venv

---
+++ projDir/django-react-venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\_workspace\web_crawler_works\git-space\projdir\django-react-venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
